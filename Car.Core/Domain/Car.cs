13458c146074224606e7ca61cf9144a196871b55
irydmy13 <kotiukir@gmail.com>
2026-01-14 11:08:21 +0200

    Added Car domain entity

---
+++ Car.Core/Domain/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car.Core.Domain
+{
+    public class Car
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public bool IsUsed { get; set; } 
+
+    
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
---
+++ Car.Core/Dto/CarDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Car.Core.Dto
+{
+    internal class CarDto
+    {
+    }
+}
